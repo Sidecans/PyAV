b9db60749c361bb1e2f3f86f47934847c1a326fb
Sidecans <sidbhave@hotmail.com>
2022-09-12 18:09:48 -0700

    Update Win11_PyAV.py

--- Win11_PyAV.py
+++ Win11_PyAV.py
@@ -1,5 +1,14 @@
 import os
-var = str(os.getcwd)
-var = var.split("\\")
-var = var[0]
-print(str(var))
+
+drive = str(os.getcwd()).split("\\")
+drive = drive[0]
+
+def scan(name):
+    print("Virus Detected!!!")
+    print("Threat Name: " + name)
+    
+
+
+if os.path.exists(drive + "\\Windows\\automks.exe"):
+    pass
+
